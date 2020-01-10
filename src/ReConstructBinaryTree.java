d5626914e5070d7b4b9c5bd48d4ae9802145ca04
zk <1164698520zk@gmail.com>
2020-01-10 10:56:36 +0800

    学习

--- src/ReConstructBinaryTree.java
+++ src/ReConstructBinaryTree.java
@@ -12,21 +12,30 @@ public class ReConstructBinaryTree {
         int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
         int[] in = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
         TreeNode treeNode = reConstructBinaryTree(pre, in);
-        ;
+
     }
 
-    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        if (pre.length == 0 || in.length == 0) {
+
+    private static TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        TreeNode root=reConstructBinaryTree(pre,0,pre.length-1,in,0,in.length-1);
+        return root;
+    }
+
+    private static TreeNode reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
+
+        if(startPre>endPre||startIn>endIn) {
             return null;
         }
-        TreeNode root = new TreeNode(pre[0]);
-        for (int i = 0; i < in.length; i++) {
-            if (in[i] == pre[0]) {
-                root.setLeft(reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1), Arrays.copyOfRange(in, 0, i)));
-                root.setRight(reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length), Arrays.copyOfRange(in, i + 1, in.length)));
+        TreeNode root=new TreeNode(pre[startPre]);
+
+        for(int i=startIn;i<=endIn;i++) {
+            if(in[i]==pre[startPre]){
+                root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
+                root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
                 break;
             }
         }
+
         return root;
     }
 }
@@ -35,23 +44,6 @@ class TreeNode {
     public int val;
     TreeNode left;
     TreeNode right;
-
-    public TreeNode getLeft() {
-        return left;
-    }
-
-    public TreeNode getRight() {
-        return right;
-    }
-
-    public void setLeft(TreeNode left) {
-        this.left = left;
-    }
-
-    public void setRight(TreeNode right) {
-        this.right = right;
-    }
-
     TreeNode(int x) {
         val = x;
     }
